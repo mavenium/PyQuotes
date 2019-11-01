b264d1dfb8c6edd869da263786859fc17ed87f50
Mehdi Namaki <mavenium@gmail.com>
2019-11-01 17:44:24 +0330

    add Category and Quote model to admin panel

--- Web/admin.py
+++ Web/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Person
+from .models import Person, Category, Quote
 
 # Register your models here.
 admin.site.register(Person)
+admin.site.register(Category)
+admin.site.register(Quote)
