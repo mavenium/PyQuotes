60ce3fbedb7bd487af08b8fadcede81f1c58a939
Mehdi Namaki <mavenium@gmail.com>
2019-11-01 20:52:03 +0330

    fix media and static DIR-ROOT

--- PyQuotes/settings.py
+++ PyQuotes/settings.py
@@ -120,3 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILE_DIRS = (
+    os.path.join(BASE_DIR, 'static/')
+)
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+MEDIA_URL = os.path.join(BASE_DIR, 'media/')
--- Web/models.py
+++ Web/models.py
@@ -4,15 +4,24 @@ from django.db import models
 class Person(models.Model):
     full_name = models.CharField(max_length=256, verbose_name="Full Name :")
     biography = models.TextField(verbose_name="Biography :")
-    picture = models.FileField(verbose_name="Picture :")
+    picture = models.FileField(verbose_name="Picture :", upload_to='persons/')
+
+    def __str__(self):
+        return self.full_name
 
 
 class Category(models.Model):
     title = models.CharField(max_length=100, verbose_name="Category Title :")
     # slug = models.SlugField(max_length=100, verbose_name="Category Slug :")
 
+    def __str__(self):
+        return self.title
+
 
 class Quote(models.Model):
     content = models.TextField(verbose_name="Quote Content :")
     person = models.OneToOneField(Person, on_delete=models.CASCADE)
     category = models.OneToOneField(Category, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        pass
