44cb7a12620158755497b704c6475d370b433f2c
Mehdi Namaki <mavenium@gmail.com>
2019-11-01 17:42:45 +0330

    add Category and Quote model

--- Web/models.py
+++ Web/models.py
@@ -5,3 +5,14 @@ class Person(models.Model):
     full_name = models.CharField(max_length=256, verbose_name="Full Name :")
     biography = models.TextField(verbose_name="Biography :")
     picture = models.FileField(verbose_name="Picture :")
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=100, verbose_name="Category Title :")
+    # slug = models.SlugField(max_length=100, verbose_name="Category Slug :")
+
+
+class Quote(models.Model):
+    content = models.TextField(verbose_name="Quote Content :")
+    person = models.OneToOneField(Person, on_delete=models.CASCADE)
+    category = models.OneToOneField(Category, on_delete=models.DO_NOTHING)
