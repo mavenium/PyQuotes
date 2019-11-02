cc74b63fa972b56c3ccddd4e3225a40a726e7c77
Mehdi Namaki <mavenium@gmail.com>
2019-11-02 22:32:35 +0330

    show Person and Category (data) in index

--- Web/views.py
+++ Web/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.views import generic
 
-from Web.models import Quote
+from Web.models import Quote, Person, Category
 
 
 # Create your views here.
 class Index(generic.ListView):
     model = Quote
     template_name = 'index.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['persons'] = Person.objects.all()
+        context['categories'] = Category.objects.all()
+        return context
