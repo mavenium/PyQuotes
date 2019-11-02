ad2d8c531b8435fca823274a6cf7a57d3db38e32
Mehdi Namaki <mavenium@gmail.com>
2019-11-02 13:09:21 +0330

    add app name to prefix of models

--- PyQuotes/settings.py
+++ PyQuotes/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Web'
+    'Web.apps.WebConfig'
 ]
 
 MIDDLEWARE = [
--- Web/admin.py
+++ Web/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Person, Category, Quote
+from Web.models import Person, Category, Quote
 
 # Register Person model
 admin.site.register(Person)
