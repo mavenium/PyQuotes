a0fed0cc43a05e634a83faf814d74e562eb240e2
Mehdi Namaki <mavenium@gmail.com>
2019-11-02 18:59:51 +0330

    fix STATICFILE_DIRS

--- PyQuotes/settings.py
+++ PyQuotes/settings.py
@@ -121,9 +121,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILE_DIRS = (
-    os.path.join(BASE_DIR, 'static/')
-)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
