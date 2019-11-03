0a81cd173fe6575975513803109f9fd2371511a9
Mehdi Namaki <mavenium@gmail.com>
2019-11-03 17:58:52 +0330

    create Master view and extend other views from Master

--- Web/views.py
+++ Web/views.py
@@ -5,13 +5,21 @@ from Web.models import Quote, Person, Category
 
 
 # Create your views here.
-class Index(generic.ListView):
-    model = Quote
-    paginate_by = 1
-    template_name = 'index.html'
-
+class Master(generic.ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         context['persons'] = Person.objects.all()
         context['categories'] = Category.objects.all()
         return context
+
+
+class Index(Master):
+    model = Quote
+    paginate_by = 1
+    template_name = 'index.html'
+
+
+class Persons(Master):
+    model = Person
+    paginate_by = 1
+    template_name = 'persons.html'
