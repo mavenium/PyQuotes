d665d184c9ad61ba291b4f0508edc6780fd37535
Mehdi Namaki <mavenium@gmail.com>
2019-11-03 20:14:41 +0330

    add path to QuotesByPerson

--- Web/urls.py
+++ Web/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, re_path
 
-from Web.views import Index, Persons
+from Web.views import Index, Persons, QuotesByPerson
 
 urlpatterns = [
     path('', Index.as_view(), name='index'),
     path('persons/', Persons.as_view(), name='persons'),
+    path('<person_pk>/', QuotesByPerson.as_view(), name='quotes_by_person'),
 ]
