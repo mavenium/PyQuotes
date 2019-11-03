977722f820e5b3a596da6f9d5111abcf19e99965
Mehdi Namaki <mavenium@gmail.com>
2019-11-03 17:57:05 +0330

    fix media settings

--- PyQuotes/settings.py
+++ PyQuotes/settings.py
@@ -120,11 +120,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
-MEDIA_URL = os.path.join(BASE_DIR, 'media/')
+
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+# MEDIA_URL = os.path.join(BASE_DIR, 'media/')
