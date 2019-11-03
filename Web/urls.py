912e2f292ea06c6780a9998663f17d84198ddc73
Mehdi Namaki <mavenium@gmail.com>
2019-11-03 17:58:12 +0330

    add persons views to url

--- Web/urls.py
+++ Web/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, re_path
 
-from Web.views import Index
+from Web.views import Index, Persons
 
 urlpatterns = [
     path('', Index.as_view(), name='index'),
+    path('persons/', Persons.as_view(), name='persons'),
 ]
