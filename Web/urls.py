d348c3410a55378216fc7411e81892696d1b0d85
Mehdi Namaki <mavenium@gmail.com>
2019-11-03 20:36:08 +0330

    fix url and add path for QuotesByCategory filter

--- Web/urls.py
+++ Web/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, re_path
 
-from Web.views import Index, Persons, QuotesByPerson
+from Web.views import Index, Persons, QuotesByPerson, QuotesByCategory
 
 urlpatterns = [
     path('', Index.as_view(), name='index'),
     path('persons/', Persons.as_view(), name='persons'),
-    path('<person_pk>/', QuotesByPerson.as_view(), name='quotes_by_person'),
+    path('qbp/<person_pk>/', QuotesByPerson.as_view(), name='quotes_by_person'),
+    path('qbc/<category_pk>/', QuotesByCategory.as_view(), name='quotes_by_category'),
 ]
