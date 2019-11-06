1ea067f22528b22204d25157723c1f9dabdc7a24
Mehdi Namaki <mavenium@gmail.com>
2019-11-06 18:44:07 +0330

    create and add API router

--- Web/urls.py
+++ Web/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 
-from Web.views import Index, Persons, Random, QuotesByPerson, QuotesByCategory
+from rest_framework import routers
+
+from Web.views import Index, Persons, Random, QuotesByPerson, QuotesByCategory, APIPersons
+
+router = routers.DefaultRouter()
+router.register(r'persons', APIPersons)
 
 urlpatterns = [
     path('', Index.as_view(), name='index'),
@@ -8,4 +13,5 @@ urlpatterns = [
     path('qbp/<person_pk>/', QuotesByPerson.as_view(), name='quotes_by_person'),
     path('qbc/<category_pk>/', QuotesByCategory.as_view(), name='quotes_by_category'),
     path('random/', Random.as_view(), name='random'),
+    path('api/', include(router.urls)),
 ]
