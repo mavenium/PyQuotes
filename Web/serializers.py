a945dac1abbde7864ac9a1cc2aadd3a68661382b
Mehdi Namaki <mavenium@gmail.com>
2019-11-06 18:43:46 +0330

    create and add PersonSerializer

---
+++ Web/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from Web.models import Person, Category, Quote
+
+
+class PersonSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Person
+        fields = ['full_name', 'biography', 'picture', 'pk']
