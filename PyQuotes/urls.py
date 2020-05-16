2fbf534c01d66ea9bf02ce82e760ad1742dcd057
mavenium <mavenium@gmail.com>
2020-05-16 10:13:22 +0430

    add accounts urls

--- PyQuotes/urls.py
+++ PyQuotes/urls.py
@@ -20,5 +20,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include("django.contrib.auth.urls")),
     path('', include('Web.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
